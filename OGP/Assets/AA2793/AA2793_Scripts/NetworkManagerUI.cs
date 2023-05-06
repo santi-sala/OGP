18ce4d809eddbd1eff8dcc30aadb81b97b0d938e
Santiago Sala <pipo0716@gmail.com>
2023-05-06 06:03:59 +0300

    Minor changes

--- OGP/Assets/AA2793/AA2793_Scripts/NetworkManagerUI.cs
+++ OGP/Assets/AA2793/AA2793_Scripts/NetworkManagerUI.cs
@@ -1,12 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Netcode;
+using System.Linq;
+using System.Threading.Tasks;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+using Unity.Services.Core;
+using Unity.Services.Authentication;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Http;
+using Unity.Services.Relay.Models;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport;
+using Unity.Networking.Transport.Relay;
+using NetworkEvent = Unity.Networking.Transport.NetworkEvent;
 
 public class NetworkManagerUI : MonoBehaviour
 {
+    const int MAX_CONNECTIONS = 4;
+
+    //public string RelayJoinCode;
+
     [SerializeField] private GameObject _connectBTNS;
     [SerializeField] private GameObject _disconnectBTN;
 
@@ -14,6 +28,7 @@ public class NetworkManagerUI : MonoBehaviour
 //#if UNITY_SERVER && !UNITY_EDITOR
     public void Start()
     {
+        Example_AuthenticatingAPlayer();
         NetworkManager.Singleton.OnClientConnectedCallback += OnClientDisconnectCallback;
     }
 //#else
@@ -35,13 +50,13 @@ public class NetworkManagerUI : MonoBehaviour
    
     public void StartHost()
     {
-        NetworkUIElementsVisibility(false, true);
-        NetworkManager.Singleton.StartHost();
+        Debug.Log("Sup");
+        StartCoroutine("StartHostCallback");        
     }
 
     public void StartClient()
     {
-        PlayerCountServerRpc();
+        //PlayerCountServerRpc();
 
         NetworkUIElementsVisibility(false, true);
         NetworkManager.Singleton.StartClient();
@@ -63,9 +78,29 @@ public class NetworkManagerUI : MonoBehaviour
         _disconnectBTN.SetActive(disconnectBTN);
     }
 
+    async void Example_AuthenticatingAPlayer()
+    {
+        try
+        {
+            await UnityServices.InitializeAsync();
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+            var playerID = AuthenticationService.Instance.PlayerId;
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e);
+        }
+    }
+    /*
     [ServerRpc(RequireOwnership = false)]
-    private bool PlayerCountServerRpc() 
-    { 
+    private void PlayerCountServerRpc() 
+    {
+        PlayerCountClientRpc();
+    }
+
+    [ClientRpc]
+    private bool PlayerCountClientRpc()
+    {
         int clientCount = NetworkManager.Singleton.ConnectedClientsIds.Count;
         Debug.Log("Im here!!!");
 
@@ -77,6 +112,58 @@ public class NetworkManagerUI : MonoBehaviour
         {
             return true;
         }
+    }*/
+
+    public static async Task<RelayServerData> AllocateRelayServerAndGetJoinCode(int maxConnections, string region = null)
+    {
+        Allocation allocation;
+        string createJoinCode;
+        try
+        {
+            allocation = await RelayService.Instance.CreateAllocationAsync(maxConnections, region);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"Relay create allocation request failed {e.Message}");
+            throw;
+        }
+
+        Debug.Log($"server: {allocation.ConnectionData[0]} {allocation.ConnectionData[1]}");
+        Debug.Log($"server: {allocation.AllocationId}");
+
+        try
+        {
+            createJoinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+            Debug.Log(createJoinCode);
+        }
+        catch
+        {
+            Debug.LogError("Relay create join code request failed");
+            throw;
+        }
+
+        return new RelayServerData(allocation, "dtls");
     }
-    //#endif
+
+    private IEnumerator StartHostCallback()
+    {
+        
+        Task<RelayServerData> relayTask = AllocateRelayServerAndGetJoinCode(MAX_CONNECTIONS);
+        
+        while(!relayTask.IsCompleted)
+        {
+            yield return null;
+        }
+
+        RelayServerData serverData = relayTask.Result;
+
+        NetworkUIElementsVisibility(false, true);
+        NetworkManager.Singleton.StartHost();
+
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(serverData);
+        NetworkManager.Singleton.StartHost();
+    }
+
+
+//#endif
 }
